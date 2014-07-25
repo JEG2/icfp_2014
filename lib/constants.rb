8925f4cf3fb7c83f1c5d52c7ea131257f078dcdb
tnordloh <tnordloh@gmail.com>
2014-07-24 23:58:07 -0500

    commit constants

---
+++ lib/constants.rb
@@ -0,0 +1,14 @@
+
+DIRECTIONS = { up:0,
+               right:1,
+               down:2,
+               left:3
+             }
+MAP = { wall:0,
+        empty:1,
+        pill:2,
+        power_pill:3,
+        fruit:4,
+        lambda_man_starting_position:5,
+        ghost_starting_position:6
+      }
