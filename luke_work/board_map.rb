c592d17fb87205f9aba610b456a739798129d495
Luke DeWitt <luke@dewittsoft.com>
2014-07-25 13:16:36 -0600

    Starting Heat Map.

---
+++ luke_work/board_map.rb
@@ -0,0 +1,18 @@
+module HeatMap
+  class BoardMap
+    
+    def initialize
+      @map_file = "board.dat"
+      @map_data = []
+    end
+    
+    attr_reader :map_file, :map_data
+
+    def readmap
+      map = File.open(map_file)
+      @map_data = eval(map.read.gsub(/\s+/,""))
+      map.close
+      @map_data
+    end
+  end
+end
