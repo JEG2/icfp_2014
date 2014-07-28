f0ce8932c066431e09296389312d23bc70b720f0
James Edward Gray II <james@graysoftinc.com>
2014-07-27 21:34:01 -0500

    Moving to bin.

--- bin/package_submission.rb
+++ bin/package_submission.rb
@@ -1,18 +1,18 @@
 #!/usr/bin/env ruby
+
 require 'fileutils'
-include FileUtils
 
 if File.exists?('submission')
   FileUtils.rm_rf 'submission'
 end
 
-FileUtils.mkdir('submission')
+FileUtils.mkdir 'submission'
 
 FileUtils.cp_r './', '../submission'
 
 FileUtils.mv '../submission', './submission/code'
 
-FileUtils.mkdir('submission/solution')
+FileUtils.mkdir 'submission/solution'
 
 ARGV.each do |arg|
   # puts "#{arg}"
